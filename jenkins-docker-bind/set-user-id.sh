a64ca59455567151a4bf27bc304224e8e4205c23
bobergda <bobergda@gmail.com>
2021-06-25 06:57:23 +0000

    add script to set ids

---
+++ jenkins-docker-bind/set-user-id.sh
@@ -0,0 +1,5 @@
+#!/bin/bash -x
+USERID=$(id -u)
+DOCKER_GID=$(cut -d: -f3 < <(getent group docker))
+echo Set $USERID:$DOCKER_GID
+sed -i "s/^\"* *user:[^$]*/    user: $USERID:$DOCKER_GID/" docker-compose.yml
