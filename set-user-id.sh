65faa3b0ce1146fef487051c4afc8150a96145f2
bobergda <bobergda@gmail.com>
2022-07-22 12:26:15 +0000

    change dir

--- set-user-id.sh
+++ set-user-id.sh
@@ -3,5 +3,5 @@ USERID=$(id -u)
 DOCKER_GID=$(cut -d: -f3 <(getent group docker))
 echo Set $USERID:$DOCKER_GID
 sed -i "s/^\"* *user:[^$]*/    user: $USERID:$DOCKER_GID/" docker-compose.yml
-mkdir ~/jenkins
-sudo chown -R $USERID:$DOCKER_GID ~/jenkins
+mkdir jenkins_home/
+sudo chown -R $USERID:$DOCKER_GID jenkins_home/
