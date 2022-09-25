6131af0f50998e88058e40fb1a2dbbd89b1f86a4
bobergda <bobergda@gmail.com>
2022-09-25 10:25:14 +0000

    fix user id

--- set-user-id.sh
+++ set-user-id.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -x
-USERID=$(id -u)
+USERID=1000
 DOCKER_GID=$(cut -d: -f3 <(getent group docker))
 echo Set $USERID:$DOCKER_GID
 sed -i "s/^\"* *user:[^$]*/    user: $USERID:$DOCKER_GID/" docker-compose.yml
